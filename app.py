7f8841ed4200ef011bbd5ee5175d8f13a7a847cb
balamaru <balamaru>
2025-08-01 06:34:51 +0000

    delete fix tag for image in deployment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "ini aplikasi versi ke 3"
+    return "ini aplikasi versi ke 2"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
